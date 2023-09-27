ef3af52ca80092d335914fab20c1333933158968
Martinagracy28 <martinagracy28@gmail.com>
2023-09-27 13:12:52 +0530

    Chaged the black price condition

--- Bond/Bond.sol
+++ Bond/Bond.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '../../NewFraxProtocol/contracts/Frax/Frax.sol';
+import '../../NewFraxProtocol/contracts/Frax/Pools/FraxPoolV3.sol';
 
 interface ITreasuryContract {
     function owner() external view returns (address);
@@ -33,7 +33,7 @@ contract BondContract {
     IERC20 public daiToken;
     IERC20 public dimeToken;
    
-    FRAXStablecoin public  JUSD;
+    FraxPoolV3 public  JUSD;
     ITreasuryContract  public  Treasury;
     AggregatorV3Interface public priceFeedDAIUSD; 
   
@@ -72,11 +72,11 @@ contract BondContract {
         owner = msg.sender;
         daiToken = IERC20(_daiTokenAddress);
         dimeToken = IERC20(_dimeTokenAddress);
-        JUSD = FRAXStablecoin(0xeeaeCf2Adb6Ae4fDf9c0988c6349cE36a8f21423);
-        priceFeedDAIUSD = AggregatorV3Interface(_priceFeedDAIUSD);        
+        JUSD = FraxPoolV3(0x3835EBeAb162061cA13407963BCe0030CC2B641f);
+        priceFeedDAIUSD = AggregatorV3Interface(_priceFeedDAIUSD);
+        chainlink_dai_usd_decimals = priceFeedDAIUSD.decimals();        
         timeDuration = _timeDuration;
         epochHours = _epochHours;
-        chainlink_dai_usd_decimals = priceFeedDAIUSD.decimals(); 
 
     }
 
@@ -96,11 +96,8 @@ contract BondContract {
         (uint80 roundID, int price, , uint256 updatedAt, uint80 answeredInRound) = priceFeedDAIUSD.latestRoundData();
         require(price >= 0 && updatedAt != 0 && answeredInRound >= roundID, "Invalid chainlink price");
 
-        uint256 getDAIPrice = uint256(price).mul(PRICE_PRECISION).mul(PRICE_PRECISION).div(uint256(10) ** chainlink_dai_usd_decimals);
+        return uint256(price).mul(PRICE_PRECISION).div(uint256(10) ** chainlink_dai_usd_decimals);
     
-        uint256 price_vs_eth = PRICE_PRECISION.mul(PRICE_PRECISION).mul(PRICE_PRECISION).div(getDAIPrice);
-
-        return JUSD.eth_usd_price().mul(PRICE_PRECISION).div(price_vs_eth);
     }
 
     // Function to deposit DAI tokens and receive DIME tokens with a 20% discount
@@ -111,7 +108,7 @@ contract BondContract {
         uint256 daiToUSDPrice = getDAIPrice(); // Implement your DAI price function
 
         // Calculate the price of 1 ELEM in USD
-        uint256 blackToUSDPrice = JUSD.black_price(); // Implement your DIME price function
+        uint256 blackToUSDPrice = JUSD.getBLACKPrice(); // Implement your DIME price function
 
         // Convert the amount from DAI to USD
         uint256 amountInUSD = amount.mul(daiToUSDPrice).div(1e6);
@@ -191,9 +188,9 @@ contract BondContract {
 
     function rebase() public {
         require((lastEpoch+epochHours) <= block.timestamp,"Epoch is not reached");
-        uint256 blackPrice = JUSD.black_price();
-        uint256 blackPriceEquilibrium = blackPrice/10000;
-        uint256 dimePrice = JUSD.fxs_price();
+        uint256 blackPrice = JUSD.getBLACKPrice();
+        uint256 blackPriceEquilibrium = blackPrice*10000;
+        uint256 dimePrice = JUSD.getFXSPrice();
         uint256 dimeSupply = dimeToken.totalSupply();
 
         
@@ -201,7 +198,13 @@ contract BondContract {
             uint256 diff_in_price = blackPriceEquilibrium - dimePrice;
             // uint256 diff = Max_threshold - blackPrice;
             //New Supply(mint) = (Old Price * Old Supply) / New Price
-            uint256 amountToMint = (dimePrice.mul(dimeSupply)).div((dimePrice.sub(diff_in_price)));
+            uint256 amountToMint;
+            if(dimePrice > diff_in_price){
+                amountToMint = (dimePrice.mul(dimeSupply)).div((dimePrice.sub(diff_in_price)));
+            }else{
+                amountToMint = (dimePrice.mul(dimeSupply)).div((diff_in_price.sub(dimePrice)));
+            }
+           
             Treasury.mintDime(amountToMint);
         }
         else if(blackPriceEquilibrium < dimePrice){
@@ -213,8 +216,7 @@ contract BondContract {
         else{
             //nothing to do
         }
-
-
+         
         lastEpoch = block.timestamp;
 
     }
